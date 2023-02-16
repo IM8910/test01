6e6b0945450d963a9b1ae0dcae58b09004ad8747
IM8910 <125559195+IM8910@users.noreply.github.com>
2023-02-16 12:52:56 +0200

    Create file2.cs
    File 2 (new br)

---
+++ file2.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal  class SampT
+    {
+        internal SampT()
+        {
+
+        }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public int Age { get; set; }
+
+        public string DoSometh()
+        {
+            string str2 = "set set 2";
+            return str2;
+        }
+    }
+}
